d193aa309ca9f1874f458c7793c9951393d152bc
paalzoltanweb <paal.donat@gmail.com>
2026-04-11 23:20:34 +0200

    Stop request

--- display/src/App.vue
+++ display/src/App.vue
@@ -2,19 +2,23 @@
 import Header from '@/components/Header.vue';
 import Footer from './components/Footer.vue';
 import Stop from './components/Stop.vue';
+import StopRequestIndicator from './components/StopRequestIndicator.vue';
 </script>
 
 <template>
   <div class="display">
     <Header/>
-    <div class="stops">
-      <div class="upcoming_stops">
-        <Stop name="Taksony, községháza" arrival="14:45" :is_last="true"/>
-        <Stop name="Dunavarsány, vasútálomás" arrival="14:05" :is_last="false"/>
-        <Stop name="Délegyháza, II. tó" arrival="13:50" :is_last="false"/>
-        <Stop name="Délegyháza, vasútálomás dkjfjhgdsjkgjsdfkg sdfg " arrival="13:42" departure="13:45" :is_last="false"/>
+    <div class="stop_area">
+      <div class="stops">
+        <div class="upcoming_stops">
+          <Stop name="Taksony, községháza" arrival="14:45" :is_last="true"/>
+          <Stop name="Dunavarsány, vasútálomás" arrival="14:05" :is_last="false"/>
+          <Stop name="Délegyháza, II. tó" arrival="13:50" :is_last="false"/>
+          <Stop name="Délegyháza, vasútálomás dkjfjhgdsjkgjsdfkg sdfg " arrival="13:42" departure="13:45" :is_last="false"/>
+        </div>
+        <Stop name="Délegyháza, vasútálomás dkjfjhgdsjkgjsdfkg sdfg " arrival="13:42" departure="13:45" :is_last="false" :is_active="true"/>
       </div>
-      <Stop name="Délegyháza, vasútálomás dkjfjhgdsjkgjsdfkg sdfg " arrival="13:42" departure="13:45" :is_last="false" :is_active="true"/>
+      <StopRequestIndicator status="none"/>
     </div>
     <Footer/>
   </div>
@@ -29,8 +33,8 @@ import Stop from './components/Stop.vue';
   flex-direction: column;
 }
 
-.display .stops {
+.stop_area {
   flex: 1;
-  overflow: auto;
+  display: flex;
 }
 </style>
---
+++ display/src/components/StopRequestIndicator.vue
@@ -0,0 +1,61 @@
+<script setup>
+const props = defineProps({
+  status: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div :class="{stop_request_indicator: true, status_none: props.status == 'none', status_stop: props.status == 'stop', status_wheelchair: props.status == 'wheelchair'}">
+    <img class="img_stop" src="/assets/img/stop_request.png" alt="STOP">
+    <img class="img_wheelchair" src="/assets/img/stop_request_wheelchair.png" alt="STOP-ACC-">
+  </div>
+</template>
+
+<style scoped>
+.stop_request_indicator {
+  border: 0.5rem solid white;
+  border-radius: 2rem;
+  display: block;
+  margin: auto 1rem 1rem 1rem;
+}
+
+.stop_request_indicator img {
+  width: 6rem;
+  margin: 1rem;
+}
+
+.stop_request_indicator.status_none {
+  opacity: 0.2;
+}
+
+.stop_request_indicator.status_none .img_wheelchair {
+  display: none;
+}
+
+.stop_request_indicator.status_stop {
+  opacity: 1;
+  background-color: red;
+  border-color: transparent;
+}
+
+.stop_request_indicator.status_stop .img_wheelchair {
+  display: none;
+}
+
+.stop_request_indicator.status_wheelchair {
+  opacity: 1;
+  background-color: hsl(220, 100%, 50%);
+  border-color: transparent;
+}
+
+.stop_request_indicator.status_wheelchair .img_wheelchair {
+  display: block;
+}
+
+.stop_request_indicator.status_wheelchair .img_stop {
+  display: none;
+}
+</style>
